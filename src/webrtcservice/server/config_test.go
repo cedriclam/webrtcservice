b97372c5ac7cd5996fbd5ab8221fc33083c121d4
Cedric Lamoriniere <cedric.lamoriniere@gmail.com>
2016-05-24 01:39:30 +0200

    Fix Travis-ci build

--- src/webrtcservice/server/config_test.go
+++ src/webrtcservice/server/config_test.go
@@ -5,9 +5,9 @@ import "testing"
 func TestGetAddr(t *testing.T) {
 	c := &Config{
 		Host: "127.0.0.1",
-		Port: 8080,
+		Port: 32000,
 	}
-	if res := c.getAddr(); res != "127.0.0.1:8080" {
+	if res := c.getAddr(); res != "127.0.0.1:32000" {
 		t.Error("wrong getAddr, current:", res)
 	}
 }
